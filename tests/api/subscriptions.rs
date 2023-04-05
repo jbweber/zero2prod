b7e340a981cd94bebdb43d024c364cc6672eb266
Jeff Weber <jweber@cofront.net>
2023-04-05 15:51:38 -0400

    errors before thiserror

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -1,9 +1,8 @@
+use crate::helpers::spawn_app;
 use wiremock::{
     matchers::{method, path},
     Mock, ResponseTemplate,
 };
-
-use crate::helpers::spawn_app;
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
@@ -136,4 +135,23 @@ async fn subscribe_sends_a_confirmation_email_with_a_link() {
     let confirmation_links = app.get_confirmation_links(&email_request);
     // The two links should be identical
     assert_eq!(confirmation_links.html, confirmation_links.plain_text);
+}
+
+#[tokio::test]
+async fn subscribe_fails_if_there_is_a_fatal_database_error() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    // Sabatoge the database
+    sqlx::query!("ALTER TABLE subscription_tokens DROP COLUMN subscription_token;")
+        .execute(&app.db_pool)
+        .await
+        .unwrap();
+
+    // Act
+    let response = app.post_subscriptions(body.into()).await;
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 500);
 }
